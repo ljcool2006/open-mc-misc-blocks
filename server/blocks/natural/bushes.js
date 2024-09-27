1807eb05e27b4edab05cd071fc9c9d4ee9e189fe
ljcool2006 <51834343+ljcool2006@users.noreply.github.com>
2024-09-27 05:49:04 -0400

    Add files via upload

---
+++ server/blocks/natural/bushes.js
@@ -0,0 +1,20 @@
+import { Items } from "../../items/item.js"
+import { peekdown, place } from "../../misc/ant.js"
+import { Block, Blocks } from "../block.js"
+
+class Bush extends Block{
+	static solid = false
+	static targettable = true
+	static breaktime = 0
+	update(){
+		if(!peekdown().dirt) this.destroy(), place(Blocks.air)
+	}
+}
+
+Blocks.dandelion = class extends Bush{
+	//drops(){ return new Items.dandelion(1) }
+}
+
+Blocks.rose = class extends Bush{
+	//drops(){ return new Items.rose(1) }
+}
