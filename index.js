e59f3ce618c1aff1c61fcb3b05938502aaa5cd4f
ljcool2006 <51834343+ljcool2006@users.noreply.github.com>
2024-09-26 23:26:57 -0400

    add rose and dandelion

---
+++ index.js
@@ -0,0 +1,17 @@
+import { Blocks, Block, BlockTexture } from 'definitions'
+import blocksPng from './blocks.png'
+
+class Bush extends Block{
+	static solid = false
+	static opacity = 0
+	static targettable = true
+	static placeSounds = Blocks.grass.placeSounds
+}
+
+Blocks.dandelion = class extends Bush{
+  static texture = BlockTexture(blocksPng, 13, 0)
+}
+
+Blocks.rose = class extends Bush{
+  static texture = BlockTexture(blocksPng, 12, 0)
+}
